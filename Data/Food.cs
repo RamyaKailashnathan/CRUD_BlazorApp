ac691f69382e7e2fd2d56082472db46c84476107
Ramya Kailashnathan <ramy0076@elev.tec.dk>
2022-10-13 13:28:32 +0200

    Add project files.

---
+++ Data/Food.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_BlazorApp.Data
+{
+    public class Stationary
+        {
+          public int Id { get; set; }
+          public string? Stationaryname { get; set; }
+          public string? Studentname { get; set; }
+  
+        }
+}
---
+++ Data/SQL.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+namespace CRUD_BlazorApp.Data
+{
+    public class SQL
+    {
+        private SqlConnection conn = new SqlConnection("Data Source=.;" +
+            "Initial Catalog=STATIONARYDB;" +
+            "User ID=sa;Password=Passw0rd;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;" +
+            "Encrypt=False;" +
+            "TrustServerCertificate=False;" +
+            "ApplicationIntent=ReadWrite;" +
+            "MultiSubnetFailover=False");
+        public List<Stationary> ReadStationary()
+        {
+            conn.Open();
+            List<Stationary> slist = new List<Stationary>();
+            SqlCommand command = new SqlCommand("Select * from [Stationary]", conn);
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Stationary s = new ()
+                    { Id = int.Parse (reader["id"].ToString()),
+                      Stationaryname = reader["sname"].ToString(),
+                      Studentname = reader["studentname"].ToString()
+                     
+                    };
+                    slist.Add(s);
+                    Console.WriteLine(String.Format("{0}", reader["id"]));
+                }
+                conn.Close();   
+                return slist;
+            }
+        }
+
+        
+    }
+}
